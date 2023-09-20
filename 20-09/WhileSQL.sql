2b052f760c9e774fc60fea0a9fdd3dd73d183164
Aquasterr <110834755+Aquasterr@users.noreply.github.com>
2023-09-20 11:17:39 -0300

    Create WhileSQL.sql

---
+++ 20-09/WhileSQL.sql
@@ -0,0 +1,22 @@
+ALTER PROCEDURE Contagem (@limite int, @tipo int)
+AS BEGIN
+	DECLARE @i int
+	if @tipo = 1 -- Regressiva
+	BEGIN
+		SET @i = @limite
+		while @i >= 1
+		BEGIN
+			SELECT Valor = @i
+			SET @i = @i - 1
+		END
+	END
+	if @tipo = 2 -- Crescente
+	BEGIN
+		SET @i = 1
+		while @i <= @limite
+		BEGIN
+			SELECT Valor = @i
+			SET @i = @i + 1
+		END
+	END
+END
